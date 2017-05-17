0e5d92ab46e9d059e2a3fedec8d497ce253742ad
Sebastian Witalec <sebawita@gmail.com>
2017-05-17 10:44:34 +0100

    Added code push

--- app/tables/tables.component.ts
+++ app/tables/tables.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Competition, LeagueTable } from '../models';
+import { CodePush, SyncStatus } from 'nativescript-code-push';
+import { isIOS } from 'platform';
 
 @Component({
   selector: 'my-tables',
   templateUrl: './tables/tables.component.html'
 })
-export class TablesComponent {
+export class TablesComponent implements OnInit {
   public competitions: Competition[] = [];
 
   public PremierLeagueId: number = 426;
@@ -17,7 +19,26 @@ export class TablesComponent {
   public Ligue1Id: number = 434;
   public EredivisieId: number = 433;
 
-  constructor() {
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.sync();
+  }
+
+  sync() {
+    const key = (isIOS) ? 'uLCcf09jhc20q9XZkgnSRHVhymsjVJtBKN2Jm' : 
+      '7ALm0ML8LI2NKhqmJ7qZqQWyV1uzVJtBKN2Jm';
+
+    CodePush.sync({deploymentKey: key},
+      (syncStatus: SyncStatus) => {
+        // alert(syncStatus);
+        if(syncStatus === SyncStatus.UPDATE_INSTALLED) {
+          alert('Please restart your application');
+        }
+      }
+    
+    )
   }
 
   private onTeamTap(teamId: number) {
